e449995f5a1ed8eaa3ad771f4fba7221c8f877f4
Kirill Vasyukov <kvasyukov99@gmail.com>
2025-01-06 12:21:00 +0500

    Variables init

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+const msg string = `Hello World!`
+
 func main() {
-	fmt.Println(`Hello world!`)
+	fmt.Println(msg)
+	var smsg string = `Hello again!`
+	smsg = `Rewrite Hello again!`
+	fmt.Println(smsg)
+	tmsg := `Hello again! Again!`
+	fmt.Println(tmsg)
 }
